b181e67d291b08497c10c62d4afbb507fcc8ea3a
huzongyao <hzy3774@gmail.com>
2019-05-08 18:22:08 +0800

    update gradle version

--- app/src/main/java/com/hzy/weex/frame/activity/SplashActivity.java
+++ app/src/main/java/com/hzy/weex/frame/activity/SplashActivity.java
@@ -2,22 +2,20 @@ package com.hzy.weex.frame.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 
-import com.airbnb.lottie.LottieAnimationView;
 import com.alibaba.android.arouter.facade.annotation.Route;
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.hzy.weex.frame.R;
 import com.hzy.weex.frame.constant.RouterHub;
-import com.hzy.weex.frame.widget.AutoWidthImageView;
 
 @Route(path = RouterHub.SPLASH_ACTIVITY)
 public class SplashActivity extends AppCompatActivity {
 
     private static boolean isSplashPlayed = false;
-    private AutoWidthImageView mBackImage;
-    private LottieAnimationView mLottieView;
+    private View mSplashText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +24,7 @@ public class SplashActivity extends AppCompatActivity {
             startWxPageAndFinish();
         } else {
             setContentView(R.layout.activity_splash);
-            mBackImage = findViewById(R.id.image_back);
-            mLottieView = findViewById(R.id.lottie_view);
-            mLottieView.useHardwareAcceleration();
+            mSplashText = findViewById(R.id.splash_image);
             startWXPageDelay();
             isSplashPlayed = true;
         }
@@ -50,7 +46,7 @@ public class SplashActivity extends AppCompatActivity {
                 startWxPageAndFinish();
             }
         });
-        mBackImage.setAnimation(animation);
+        mSplashText.setAnimation(animation);
     }
 
     private void startWxPageAndFinish() {
